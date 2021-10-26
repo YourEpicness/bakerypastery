761ad8d5d4956f5e2e1ef33c042b4d053aae3c10
Julaam Diop <jeebe.diop@gmail.com>
2021-10-26 03:19:03 -0500

    Adjust styling for banner component

--- components/banner.tsx
+++ components/banner.tsx
@@ -8,12 +8,12 @@ type Props = {
 
 const Banner: NextPage<Props> = (props) => {
   return (
-    <div>
-      <h1 className="text-xl">
-        <span className="font-bold">Bakery/</span>
-        <span className="text-white">Pastery</span>
+    <div className="w-full text-white">
+      <h1 className="text-4xl font-bold w-full mb-5">
+        <span className="">Bakery</span>
+        <span className="text-blue-800">Pastery</span>
       </h1>
-      <p className=""> Discover your local bakeries!</p>
+      <p className="text-lg mb-2"> Discover your local bakeries!</p>
       <button className="btn-hero" onClick={props.handleOnClick}>
         {props.buttonText}
       </button>
