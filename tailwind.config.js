01079b1d493f208fe3261844c090692dfe976f35
Julaam Diop <jeebe.diop@gmail.com>
2021-10-26 02:00:50 -0500

    Create, configure, and integrate a mesh background in tailwind and add to body

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
+  mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
       sans: ["Helvetica", "Arial", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "mesh-bg": "url('/static/background.png')",
+      },
+    },
   },
   variants: {
     extend: {},
