604ba6bde09f0bbbceaffe67abbba94eeda55f81
Julaam Diop <jeebe.diop@gmail.com>
2021-10-26 01:31:41 -0500

    Create a footer and add custom font family in theems

---
+++ components/footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import type { NextPage } from "next";
+
+const Footer: NextPage = () => {
+  return (
+    <footer className="bg-black">
+      <h3 className="text-lg text-white flex place-content-center">
+        © 2021 Diop
+      </h3>
+    </footer>
+  );
+};
+
+export default Footer;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Footer from "../components/footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="font-sans">
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
 export default MyApp;
