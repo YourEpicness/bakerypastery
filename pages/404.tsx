0cb2d8e90c144f7126b7dbb7d4a0c3dba8bec300
Julaam Diop <jeebe.diop@gmail.com>
2021-12-01 01:41:07 -0600

    Add 404 page, use fallbacks when loading, and refactor getStaticPaths to map from data

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Error: NextPage = () => {
+  return (
+    <div>
+      <h1 className=""> 404 Page Not Found</h1>
+    </div>
+  );
+};
+
+export default Error;
--- pages/bakery/[id].tsx
+++ pages/bakery/[id].tsx
@@ -2,8 +2,8 @@ import type { NextPage } from "next";
 import { GetStaticProps, GetStaticPaths } from "next";
 import { useRouter } from "next/dist/client/router";
 import Link from "next/link";
-
 import bakeryData from "../../data/coffee-stores.json";
+import Head from "next/head";
 
 export const getStaticProps: GetStaticProps = (context) => {
   const { params } = context;
@@ -18,8 +18,15 @@ export const getStaticProps: GetStaticProps = (context) => {
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = bakeryData.map((bakery) => {
+    return {
+      params: {
+        id: bakery.id.toString(),
+      },
+    };
+  });
   return {
-    paths: [{ params: { id: "1" } }, { params: { id: "2" } }],
+    paths,
     fallback: true,
   };
 };
@@ -28,16 +35,26 @@ const Bakery: NextPage<{ bakery: any }> = (props) => {
   const router = useRouter();
   console.log("router", router);
 
+  if (router.isFallback) {
+    return <div className="">Loading...</div>;
+  }
+
+  const { address, name, neighborhood } = props.bakery;
+
   console.log("props", props);
   return (
     <div>
+      <Head>
+        <title>{name}</title>
+      </Head>
       Bakery Page {router.query.id}
       <Link href="/"> Back to home</Link>
       <Link href="/bakery/dynamic">
         <a href="">Go to page dynamic</a>
       </Link>
-      <p>{props.bakery.address}</p>
-      <p>{props.bakery.name}</p>
+      <p>{address}</p>
+      <p>{name}</p>
+      <p>{neighborhood}</p>
     </div>
   );
 };
