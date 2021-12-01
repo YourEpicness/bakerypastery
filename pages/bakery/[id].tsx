ab5258b5c09ea01defe79e69d278ec8e0f801626
Julaam Diop <jeebe.diop@gmail.com>
2021-12-01 01:25:57 -0600

    Use getStaticProps and getStaticPaths to load bakery data

--- pages/bakery/[id].tsx
+++ pages/bakery/[id].tsx
@@ -1,10 +1,34 @@
-import React from "react";
+import type { NextPage } from "next";
+import { GetStaticProps, GetStaticPaths } from "next";
 import { useRouter } from "next/dist/client/router";
 import Link from "next/link";
 
-const Bakery = () => {
+import bakeryData from "../../data/coffee-stores.json";
+
+export const getStaticProps: GetStaticProps = (context) => {
+  const { params } = context;
+  console.log("params", params);
+  return {
+    props: {
+      bakery: bakeryData.find((bakeryStore) => {
+        return bakeryStore.id.toString() === params?.id; //dynamic id
+      }),
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [{ params: { id: "1" } }, { params: { id: "2" } }],
+    fallback: true,
+  };
+};
+
+const Bakery: NextPage<{ bakery: any }> = (props) => {
   const router = useRouter();
   console.log("router", router);
+
+  console.log("props", props);
   return (
     <div>
       Bakery Page {router.query.id}
@@ -12,6 +36,8 @@ const Bakery = () => {
       <Link href="/bakery/dynamic">
         <a href="">Go to page dynamic</a>
       </Link>
+      <p>{props.bakery.address}</p>
+      <p>{props.bakery.name}</p>
     </div>
   );
 };
