edc04ba084ae54e6a30318009cb31bfcc6f455c7
Julaam Diop <jeebe.diop@gmail.com>
2021-12-03 00:05:40 -0600

    Use React Context to create a store and reducer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,50 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Footer from "../components/footer";
+import { createContext, useReducer } from "react";
+
+const StoreContext = createContext({});
+
+const ACTION_TYPES = {
+  SET_LAT_LONG: "SET_LAT_LONG",
+  SET_BAKERIES: "SET_BAKERIES",
+};
+
+const storeReducer = (state: any, action: any) => {
+  switch (action.type) {
+    case ACTION_TYPES.SET_LAT_LONG: {
+      return { ...state, latLong: action.payload.latLong };
+    }
+    case ACTION_TYPES.SET_BAKERIES: {
+      return { ...state, bakeries: action.payload.bakeries };
+    }
+    default:
+      throw new Error(`Unahndled action type: ${action.type}`);
+  }
+};
+
+const StoreProvider = ({ children }: any) => {
+  const initialState = {
+    latLong: "",
+    bakeries: [],
+  };
+
+  const [state, dispatch] = useReducer(storeReducer, initialState);
+  return (
+    <StoreContext.Provider value={{ state, dispatch }}>
+      {children}
+    </StoreContext.Provider>
+  );
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="font-sans">
-      <Component {...pageProps} />
-      <Footer />
-    </div>
+    <StoreProvider>
+      <div className="font-sans">
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+    </StoreProvider>
   );
 }
 export default MyApp;
