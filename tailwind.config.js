4027015cfacab6e6334d97b90ed25e1b3bc7a5a3
Julaam Diop <jeebe.diop@gmail.com>
2022-01-07 14:44:39 -0500

    Upgrade tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 module.exports = {
-  mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fontFamily: {
-      sans: ["BalsamiqSans", "Helvetica", "Arial", "sans-serif"],
-      custom: ["BalsamiqSans"],
-    },
     extend: {
       backgroundImage: {
         "mesh-bg": "url('/static/background.png')",
+      },
+      fontFamily: {
+        sans: ["BalsamiqSans", "Helvetica", "Arial", "sans-serif"],
+        custom: ["BalsamiqSans"],
       },
     },
   },
